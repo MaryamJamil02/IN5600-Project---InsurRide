8bbf1c612f6fca89fd5b248630f0640eaa615bde
Maryam Jamil <maryamj@ulrik.uio.no>
2025-02-25 15:16:02 +0100

    Change name

--- app/src/main/java/com/example/in5600_project/MainActivity.kt
+++ app/src/main/java/com/example/in5600_project/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     IN5600ProjectTheme {
-        Greeting("Android")
+        Greeting("Maryam")
     }
 }
