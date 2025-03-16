a992c88cdbe7a001862818b6446c561be223519f
toobarana <tooba.iar@gmail.com>
2025-03-16 14:58:32 +0100

    Navigate back from ChangePasswordScreen

---
+++ app/src/main/java/com/example/in5600_project/presentation/ui/components/GoBackButton.kt
@@ -0,0 +1,18 @@
+package com.example.in5600_project.presentation.ui.components
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun GoBackButton(navController: NavController) {
+    IconButton(onClick = { navController.popBackStack() }) {
+        Icon(
+            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+            contentDescription = "Go Back"
+        )
+    }
+}
--- app/src/main/java/com/example/in5600_project/presentation/ui/screens/ChangePasswordScreen.kt
+++ app/src/main/java/com/example/in5600_project/presentation/ui/screens/ChangePasswordScreen.kt
@@ -2,7 +2,10 @@ package com.example.in5600_project.presentation.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,11 +14,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.in5600_project.presentation.ui.components.ChangePasswordButton
 import com.example.in5600_project.presentation.viewmodel.ChangePasswordScreenViewModel
 import com.example.in5600_project.presentation.viewmodel.MyProfileViewModel
 import androidx.lifecycle.viewmodel.compose.*
+import com.example.in5600_project.presentation.ui.components.GoBackButton
 
 
 @Composable
@@ -33,9 +38,11 @@ fun ChangePasswordScreen(
         modifier = modifier
             .background(Color.LightGray)
             .fillMaxSize()
+            .padding(top = 30.dp)
     )
     {
-        Text("Welcome to Change Password")
+
+        GoBackButton(navController)
 
         // Create new password
         OutlinedTextField(
